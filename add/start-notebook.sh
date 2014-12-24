b36ae1245dc2bf7f62950b6396248ee97b6eeee2
hhlin <hhlin54133@gmail.com>
2014-12-24 13:54:51 +0800

    Update start-notebook.sh

--- add/start-notebook.sh
+++ add/start-notebook.sh
@@ -1,3 +1,3 @@
 JAR_PATH="$(dirname $(dirname $(readlink -f $0)))/lib/IScala.jar"
 KERNEL_CMD="[\"java\", \"-jar\", \"$JAR_PATH\", \"--profile\", \"{connection_file}\", \"--parent\", \"$@\"]"
-ipython notebook --ip=0.0.0.0 --notebook-dir=/d/notebook  --KernelManager.kernel_cmd="$KERNEL_CMD"
+ipython notebook --ip=0.0.0.0 --notebook-dir=/d/notebook --profile=scala  --KernelManager.kernel_cmd="$KERNEL_CMD"
